620e0bbd6722673bb086f7e9d5ed9596a78ee481
JosephNjorog <njorojoe11173@gmail.com>
2024-07-06 09:37:07 +0300

    This file defines the Service model using Mongoose. The Service model is used to store information about the services that are available on the system.

---
+++ mental-synch-backend/models/Service.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ServiceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
