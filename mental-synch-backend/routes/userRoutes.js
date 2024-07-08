00693f2cdbd0aa416f38e565e32d92011642e04f
JosephNjorog <njorojoe11173@gmail.com>
2024-07-08 12:06:40 +0300

    Routes

--- mental-synch-backend/routes/userRoutes.js
+++ mental-synch-backend/routes/userRoutes.js
@@ -1,9 +1,6 @@
-// routes/userRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const auth = require('../middleware/authMiddleware');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
@@ -11,13 +8,5 @@ router.get('/auth/google', userController.googleLogin);
 router.get('/auth/google/callback', userController.googleCallback);
 router.get('/auth/facebook', userController.facebookLogin);
 router.get('/auth/facebook/callback', userController.facebookCallback);
-
-// @route   GET api/users/me
-// @desc    Get current user
-router.get('/me', auth, userController.getCurrentUser);
-
-// @route   PUT api/users/me
-// @desc    Update current user
-router.put('/me', auth, userController.updateUser);
 
 module.exports = router;
