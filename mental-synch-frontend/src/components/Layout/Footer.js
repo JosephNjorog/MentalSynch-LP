d4dab7099521c57b157e546b4d08fc236cffb644
JosephNjorog <njorojoe11173@gmail.com>
2024-07-08 13:51:58 +0300

    Components for the overall layout (Navbar, Footer).

---
+++ mental-synch-frontend/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Layout/Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2024 MentalSynch. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
