1f6b4b83a815547fcd84aa1bfb69e73123f0104e
JosephNjorog <njorojoe11173@gmail.com>
2024-07-08 11:52:20 +0300

    userController.js

--- mental-synch-backend/controllers/userController.js
+++ mental-synch-backend/controllers/userController.js
@@ -1,3 +1,44 @@
+// controllers/userController.js
+
+const User = require('../models/User');
+const passport = require('passport');
+
+exports.register = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const user = new User({ name, email });
+        await User.register(user, password);
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.login = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) return next(err);
+        if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+
+        req.logIn(user, (err) => {
+            if (err) return next(err);
+            return res.status(200).json({ message: 'Logged in successfully', user });
+        });
+    })(req, res, next);
+};
+
+exports.googleLogin = passport.authenticate('google', { scope: ['profile', 'email'] });
+
+exports.googleCallback = passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/'
+});
+
+exports.facebookLogin = passport.authenticate('facebook', { scope: ['email'] });
+
+exports.facebookCallback = passport.authenticate('facebook', {
+    successRedirect: '/home',
+    failureRedirect: '/'
+});
 const User = require('../models/User');
 
 exports.getCurrentUser = async (req, res) => {
