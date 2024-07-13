e977ec713d827d7683b7d8de9856d58d7d2c5aca
JosephNjorog <njorojoe11173@gmail.com>
2024-07-13 22:08:49 +0300

    I removed the duplicate const User = require('../models/User'); declaration. All function exports are consolidated under the single User declaration at the top of the file.

--- mental-synch-backend/controllers/userController.js
+++ mental-synch-backend/controllers/userController.js
@@ -1,3 +1,5 @@
+// controllers/userController.js
+
 const User = require('../models/User');
 const passport = require('passport');
 
@@ -37,31 +39,30 @@ exports.facebookCallback = passport.authenticate('facebook', {
     successRedirect: '/home',
     failureRedirect: '/'
 });
-const User = require('../models/User');
 
 exports.getCurrentUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 };
 
 exports.updateUser = async (req, res) => {
-  const { name, email } = req.body;
-  try {
-    let user = await User.findById(req.user.id);
-    if (!user) {
-      return res.status(404).json({ msg: 'User not found' });
+    const { name, email } = req.body;
+    try {
+        let user = await User.findById(req.user.id);
+        if (!user) {
+            return res.status(404).json({ msg: 'User not found' });
+        }
+        user.name = name || user.name;
+        user.email = email || user.email;
+        await user.save();
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
     }
-    user.name = name || user.name;
-    user.email = email || user.email;
-    await user.save();
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
 };
